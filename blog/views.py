bd99f9a9ca2585a0d68438782e600301f79ba899
Betta-Lyon-Delsordo <lyondelsordo.betta@gmail.com>
2020-09-13 23:31:34 -0600

    template files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.view.generic import ListView
+from .models import Posts
 
 # Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html"
